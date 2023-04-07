52fc1bb8053673162a1c9cd1c2ff837f7f23c858
Lục Thiên Phong <90561566+lucthienphong1120@users.noreply.github.com>
2023-04-07 12:11:10 +0700

    update

---
+++ Blank_diploma/render.js
@@ -0,0 +1,56 @@
+class CertData {
+    constructor() {
+        this.dataUrl = '/datasheet.json';
+        this.validIds = [];
+        this.urlParams = new URLSearchParams(window.location.search);
+        this.id = this.urlParams.get('id');
+        this.resource = document.getElementById('certImg');
+        this.error = document.getElementById('error-message');
+    }
+
+    async loadData() {
+        // Get the ID parameter from the URL
+        const response = await fetch(this.dataUrl);
+        const data = await response.json();
+        this.validIds = Object.keys(data);
+        return data;
+    }
+
+    // Load the data source file and render the certificate image
+    renderData() {
+        // Check if the ID is valid
+        if (this.validIds.includes(this.id)) {
+            const data = this.data[this.id];
+            // Add the data parameter to the URL if it is not already present
+            const checkData = data.slice(-20, -5);
+            if (this.urlParams.get('data') != checkData) {
+                this.urlParams.set("data", checkData)
+                window.location.search = this.urlParams;
+            }
+            // Display the appropriate certificate image
+            const source = atob(data) + '#toolbar=0';
+            this.resource.src = source;
+            return atob(data)
+        } else {
+            // Display an error message if there is an error loading the data source file
+            this.error.style.display = 'block';
+            this.resource.style.display = 'none';
+            if (this.urlParams.get('data')) {
+                this.urlParams.delete('data');
+                var newUrl = `${window.location.pathname}?${this.urlParams.toString()}`;
+                window.history.replaceState(null, null, newUrl);
+            }
+            return null;
+        }
+    }
+
+    async init() {
+        this.data = await this.loadData();
+        // Load the contents of the data source file into the validIds variable
+        this.renderData();
+    }
+}
+
+// Create an instance of the CertData class and load and render the certificate image
+let certData = new CertData();
+certData.init();
--- Blank_diploma/script.js
+++ Blank_diploma/script.js
@@ -1,7 +1,7 @@
 // check if windows in mobile mode < 800px
 if (window.innerWidth < 800) {
     const searchIcon = document.querySelector('.mobile-menu .search')
-    const menuIcon  = document.querySelector('.mobile-menu .menu')
+    const menuIcon = document.querySelector('.mobile-menu .menu')
     const searchBar = document.querySelector('.search-container')
     const menuBar = document.querySelector('nav ul')
     // on click search icon, toggle search bar
@@ -13,3 +13,82 @@ if (window.innerWidth < 800) {
         menuBar.classList.toggle('show')
     })
 }
+
+const dialog = document.querySelector('.dialog')
+const downloadBtn = document.querySelector('.download')
+const shareBtn = document.querySelector('.share')
+const closeBtn = document.querySelector('.close')
+const dialogInput = document.querySelector('.dialog-body input')
+
+// handle popup dialog
+function popupDialog(title, message, button) {
+    document.querySelector('.dialog-title h3').textContent = title
+    document.querySelector('.dialog-body p').textContent = message
+    document.querySelector('.dialog-body button').textContent = button
+}
+function shareURL() {
+    var currentURL = window.location.href;
+    // Hiển thị URL trên trang
+    dialogInput.value = currentURL;
+    document.querySelector('.dialog-body button').addEventListener("click", () => {
+        // Kiểm tra xem trình duyệt có hỗ trợ clipboard hay không
+        if (navigator.clipboard) {
+            navigator.clipboard.writeText(currentURL)
+                .then(function () {
+                    alert("URL đã được sao chép vào clipboard!");
+                })
+                .catch(function () {
+                    alert("Sao chép URL thất bại.");
+                });
+        }
+    })
+}
+function downloadURL() {
+    var pathData = certData.renderData();
+    if (pathData == null) {
+        dialogInput.value = '';
+        return
+    }
+    document.querySelector('.dialog-body button').addEventListener("click", () => {
+        // Hiển thị URL trên trang
+        const currentPath = window.location.origin + window.location.pathname;
+        const folderPath = currentPath.substring(0, currentPath.lastIndexOf('/') + 1);
+        const fullPath = folderPath.endsWith('/') ? folderPath + pathData : folderPath + '/' + pathData;
+        // Fetch PDF và tạo một Blob object
+        fetch(fullPath)
+            .then(response => response.blob())
+            .then(blob => {
+                // Tạo một URL đến Blob
+                const url = URL.createObjectURL(blob);
+                dialogInput.value = url;
+                // Mở file PDF trong tab mới
+                window.open(url, '_blank');
+            });
+    })
+}
+// on click download button, toggle dialog
+downloadBtn.addEventListener('click', () => {
+    dialog.classList.toggle('show')
+    popupDialog(
+        'Download Certificate',
+        'Enter your email address to receive a printout of your certificate.',
+        'Download'
+    )
+    dialogInput.value = '';
+    downloadURL();
+})
+// on click share button, toggle dialog
+shareBtn.addEventListener('click', () => {
+    dialog.classList.toggle('show')
+    popupDialog(
+        'Share Certificate',
+        'Share your certificate with friends to prove your competence.',
+        'Share'
+    )
+    dialogInput.value = '';
+    shareURL();
+})
+// on click close button, toggle dialog
+closeBtn.addEventListener('click', () => {
+    dialog.classList.toggle('show')
+})
