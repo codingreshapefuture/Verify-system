e20bb444465445acd032176b295a5ef4de2cde5b
Thien Phong <aichoibangbangko@gmail.com>
2025-03-09 19:09:59 +0700

    fix catch exception

---
+++ ITFB1/README.md
@@ -0,0 +1,13 @@
+## Title: ITFB - Team Truyền thông (Gen 1)
+
+> Learning code: ITFB - Team Truyền thông (Gen 1)
+
+### Certificates
+
+| ID | Demonstrated | URL |
+| --- | --- | --- |
+| 1 | Đào Thanh Long | [CMS-Dao-Thanh-Long](/verify.html?id=CMS001) |
+| 2 | Huỳnh Thị Ngọc Hân | [CMS-Huynh-Thi-Ngoc-Han](/verify.html?id=CMS002) |
+| 3 | Ngô Quỳnh Thảo Nguyên | [CMS-Ngo-Thao-Nguyen](/verify.html?id=CMS003) |
+| 4 | Trần Mẫn Mẫn | [CMS-Tran-Man-Man](/verify.html?id=CMS004) |
+| 5 | Vũ Anh Tuấn | [CMS-Vu-Anh-Tuan](/verify.html?id=CMS005) |
--- render.js
+++ render.js
@@ -21,14 +21,15 @@ class CertData {
     }
 
     async verifyData() {
-        // verify the parameter from url
-        var regex = /^([a-zA-Z]{3})(\d+)$/;
+        // verify and extract the parameter from url
+        var regex = /^([A-Z]+[0-9]*)(\d{3})$/; // Code (string/number) + ID (last 3 number)
         var matches = this.codeId.match(regex);
         if (matches) {
             this.course = matches[1];
             this.id = matches[2];
             return true;
         } else {
+            console.error("Code ID không hợp lệ!")
             return false;
         }
     }
@@ -60,56 +61,63 @@ class CertData {
                 const renderContext = { canvasContext: context, viewport: scaledViewport };
                 page.render(renderContext);
             });
-        }).catch(error => {
-            console.error("Error rendering PDF:", error);
-            this.error.style.display = 'block';
+        }).catch(e => {
+            this.showCustomError("Error while rendering PDF!");
         });
     }
 
     // Load the data source file and render the certificate image
     async renderData() {
-        // Check if the ID is valid
-        if (this.validIds.includes(this.codeId) && this.verifyData()) {
-            // Extract the cert data and other info from the data source file
-            const renderData = this.data[this.codeId];
-            // Cập nhật thông tin tiêu đề và mô tả chứng chỉ
-            document.getElementById("courseTitle").textContent = renderData.courseTitle;
-            document.getElementById("certifiedName").textContent = renderData.certifiedName;
-            // Get final data string from course and id
-            const finalData = `${this.course.toUpperCase()}/Certificates/${atob(renderData.certData)}`; // /<course>/Certificates/<cert-path>.pdf
-            // Add the data parameter to the URL if it is not already present
-            const checkData = renderData.certData.slice(-20, -5); // ?id=<uuid>&data=<encoded>
-            if (this.urlParams.get('data') != checkData) {
-                this.urlParams.set("data", checkData)
-                window.location.search = this.urlParams;
-            }
-            // Get the path to the certificate image
-            const currentPath = window.location.origin + window.location.pathname; // http://<url>/verify.html
-            const folderPath = currentPath.substring(0, currentPath.lastIndexOf('/') + 1); // http://<url>/
-            const fullPath = folderPath.endsWith('/') ? folderPath + finalData : folderPath + '/' + finalData;
+        try {
+            // Check if the ID is valid
+            if (this.validIds.includes(this.codeId) && this.verifyData()) {
+                // Extract the cert data and other info from the data source file
+                const renderData = this.data[this.codeId];
+                // Cập nhật thông tin tiêu đề và mô tả chứng chỉ
+                document.getElementById("courseTitle").textContent = renderData.courseTitle;
+                document.getElementById("certifiedName").textContent = renderData.certifiedName;
 
-            // Dùng PDF.js để render file PDF
-            this.renderPDF(fullPath);
+                // Get final data string from course and id
+                const finalData = `${this.course.toUpperCase()}/Certificates/${atob(renderData.certData)}`; // /<course>/Certificates/<cert-path>.pdf
+                // Add the data parameter to the URL if it is not already present
+                const checkData = renderData.certData.slice(-20, -5); // ?id=<uuid>&data=<encoded>
+                if (this.urlParams.get('data') != checkData) {
+                    this.urlParams.set("data", checkData)
+                    window.location.search = this.urlParams;
+                }
+                // Get the path to the certificate image
+                const currentPath = window.location.origin + window.location.pathname; // http://<url>/verify.html
+                const folderPath = currentPath.substring(0, currentPath.lastIndexOf('/') + 1); // http://<url>/
+                const fullPath = folderPath.endsWith('/') ? folderPath + finalData : folderPath + '/' + finalData;
 
-            // Fetch PDF và tạo một Blob object cho download
-            const response = await fetch(fullPath);
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
+                // Dùng PDF.js để render file PDF
+                this.renderPDF(fullPath);
+
+                // Fetch PDF và tạo một Blob object cho download
+                try {
+                    const response = await fetch(fullPath);
+                    const blob = await response.blob();
+                    this.certURL = URL.createObjectURL(blob);
+                    return this.certURL;
+                }
+                catch (e) {
+                    this.showCustomError("Network response error!");
+                    return null;
+                }
+            } else {
+                // Display an error message if there is an error loading the data source file
+                this.error.style.display = 'block';
+                document.getElementById("pdfViewer").style.display = "none";
+                if (this.urlParams.get('data')) {
+                    this.urlParams.delete('data');
+                    var newUrl = `${window.location.pathname}?${this.urlParams.toString()}`;
+                    window.history.replaceState(null, null, newUrl);
+                }
+                this.showCustomError("Invalid ID or data verification failed!");
             }
-            const blob = await response.blob();
-            this.certURL = URL.createObjectURL(blob);
-            return this.certURL;
-        } else {
-            // Display an error message if there is an error loading the data source file
-            this.error.style.display = 'block';
-            document.getElementById("pdfViewer").style.display = "none";
-            if (this.urlParams.get('data')) {
-                this.urlParams.delete('data');
-                var newUrl = `${window.location.pathname}?${this.urlParams.toString()}`;
-                window.history.replaceState(null, null, newUrl);
-            }
-            console.error("Load failed!")
-            return null;
+        } catch (error) {
+            // Xử lý lỗi theo cách riêng, không output vào console
+            this.showCustomError("Cannot load certificate. Please check the ID.");
         }
     }
 
@@ -117,6 +125,13 @@ class CertData {
         // Load the contents of the data source file into the validIds variable
         this.data = await this.loadData();
         return this.renderData()
+    }
+
+    // Hàm hiển thị lỗi tùy chỉnh
+    showCustomError(message) {
+        this.error.style.display = 'block';
+        this.error.innerHTML = `<p>${message}</p>`;
+        this.resource.style.display = 'none';
     }
 }
 
